86954f12a9a7d41cbe573ac144c59246bbe67766
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-09-24 22:41:04 +0200

    using ud to use hotreload with events

--- modules/client.js
+++ modules/client.js
@@ -3,8 +3,18 @@ console.log('link to ' + __filename);
 
 const init = require('./init');
 const fs = require('fs');
+var ud = require('ud');
 //require('aframe');
+
+var compo = {
+  init: ud.defn(module, function () {
+    console.log('I am ready!');
+  })
+};
+
+if(!module.hot.data) AFRAME.registerComponent('do-something-once-loaded', compo);
 
 var s = fs.readFileSync(__dirname + '/client.html', 'utf8');
 var e = document.getElementById('content');
 e.innerHTML = s;
+
