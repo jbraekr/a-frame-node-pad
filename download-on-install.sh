61f356e49f1b7c6e57d2c394bf9af16df8fb63ca
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-09-24 15:46:53 +0200

    animation lib lädt

--- download-on-install.sh
+++ download-on-install.sh
@@ -2,4 +2,6 @@ cd external
 curl -O https://aframe.io/releases/0.7.0/aframe.min.js
 curl -O https://unpkg.com/aframe-inspector@0.7.0/dist/aframe-inspector.min.js
 curl -O https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg
+curl -L -O https://unpkg.com/aframe-animation-component/dist/aframe-animation-component.min.js
+# -L : redirect
 ls -l
