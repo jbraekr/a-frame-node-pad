5a8d0d3139e68b642bea54a7a1d87e7c3f0e0632
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-10-05 06:19:54 +0200

    browserify-middleware moved to function for merge

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 // where your node app starts
 
 // init project
-var browserify = require('browserify-middleware');
 var express = require('express');
 var app = express();
 
@@ -14,12 +13,7 @@ app.use(express.static('views'));
 app.use(express.static('public'));
 app.use(express.static('external'));
 
-app.get('/client.js', browserify(__dirname + '/modules/client.js', {
-  plugins: [
-    { plugin: 'browserify-hmr', options: {} }
-  ],
-  transform: ['brfs']
-}));
+useBrowserifyMiddleware();
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
@@ -32,3 +26,14 @@ var port = process.env.PORT || 3000;
 var listener = app.listen(port, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+// browserify- variants
+function useBrowserifyMiddleware() {
+  const browserify = require('browserify-middleware');
+  app.get('/client.js', browserify(__dirname + '/modules/client.js', {
+    plugins: [
+      { plugin: 'browserify-hmr', options: {} }
+    ],
+    transform: ['brfs']
+  }));
+}
