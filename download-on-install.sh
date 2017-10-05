93c8046f21ecf57ae3934bf07b31d30880df091c
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-10-05 16:40:09 +0200

    download-on-install updated

--- download-on-install.sh
+++ download-on-install.sh
@@ -2,7 +2,6 @@ cd external
 curl -O https://aframe.io/releases/0.7.0/aframe.min.js
 curl -O https://unpkg.com/aframe-inspector@0.7.0/dist/aframe-inspector.min.js
 curl -O https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg
-curl -L -O https://unpkg.com/aframe-animation-component/dist/
-curl -L -O https://rawgit.com/takahirox/aframe-outline/master/build/aframe-outline.min.js
+curl -L -O https://unpkg.com/aframe-animation-component/dist/aframe-animation-component.min.js
 # -L : redirect
 ls -l
