4f8ab58933ca2c4fac0ff97c0ea7f252b1b3e86e
jbraekr <32002019+jbraekr@users.noreply.github.com>
2017-10-05 05:56:54 +0200

    watchify runs

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 // where your node app starts
 
 // init project
-var browserify = require('browserify-middleware');
+var browserify = require('browserify');
+var watchify = require('watchify');
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -13,18 +15,29 @@ var app = express();
 app.use(express.static('views'));
 app.use(express.static('public'));
 app.use(express.static('external'));
-
-app.get('/client.js', browserify(__dirname + '/modules/client.js', {
-  plugins: [
-    //{ plugin: 'brfs', options: {} }
-  ]
-}));
+app.use(express.static('build'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
   console.log('/');
   response.redirect('index.html');
 });
+
+// Build bundle https://www.npmjs.com/package/watchify
+var b = browserify({
+  cache: {}, packageCache: {},
+  entries: ['modules/client.js'],
+});
+b.plugin(watchify);
+b.transform(require('brfs'));
+b.plugin(require('browserify-hmr'));
+
+b.on('update', bundle);
+bundle();
+
+function bundle() {
+  b.bundle().pipe(fs.createWriteStream('build/client.js'));
+}
 
 // listen for requests :)
 var port = process.env.PORT || 3000;
